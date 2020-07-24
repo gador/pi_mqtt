c4b566331b47de41ce5d79b0ceea5eb41b1c9b6e
Florian Brandes <florian.brandes@posteo.de>
2020-07-24 19:21:11 +0200

    add config and mqtt broker code

--- get_temp.py
+++ get_temp.py
@@ -1,9 +1,31 @@
 #! /usr/bin/env python3
 from subprocess import check_output
 from re import findall
+import paho.mqtt.publish as publish
+import configparser
+import sys
+
+config = configparser.ConfigParser()
+config.read('pi_mqtt.conf')
+
+MQTT_USER = config['DEFAULT']['user']
+MQTT_PW = config['DEFAULT']['password']
+MQTT_BROKER = config['DEFAULT']['host']
+MQTT_TOPIC = config['DEFAULT']['topic']
+
+MQTT_AUTH = {'username':MQTT_USER, 'password':MQTT_PW}
 
 def get_temp():
-    temp = check_output(["vcgencmd","measure_temp"]).decode("UTF-8")
+    try:
+        temp = check_output(["vcgencmd","measure_temp"]).decode("UTF-8")
+    except FileNotFoundError:
+        print("Please install vcgencmd.")
+        sys.exit(1)
     return(findall("\d+\.\d+",temp)[0])
 
-print(get_temp())
+
+def publish_message(topic, message):
+    publish.single(topic, message, hostname=MQTT_BROKER, auth=MQTT_AUTH)
+
+temp = get_temp()
+publish_message(MQTT_TOPIC, temp)
